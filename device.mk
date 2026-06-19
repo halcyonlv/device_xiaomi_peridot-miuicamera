d53b85d8180af153250b1c53adcf75e43a4ae361
NotBlazeee28 <p.devansh@proton.me>
2026-06-19 15:52:34 +0000

    peridot-miuicamera: overlay: Switch to nothingOS camera icon
    - This looks almost same as halcyon aperture camera icon but in white theme
    
    Change-Id: I6dbccaf5ca35033f9e1cbfc44e1aab29891ac2a3

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ $(call inherit-product, vendor/xiaomi/peridot-miuicamera/peridot-miuicamera-vend
 # Camera
 $(call soong_config_set_bool,camera,override_format_from_reserved,true)
 
+# Overlay
+PRODUCT_PACKAGES += \
+    MiuiCameraOverlay
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries-xiaomi.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/public.libraries-xiaomi.txt
